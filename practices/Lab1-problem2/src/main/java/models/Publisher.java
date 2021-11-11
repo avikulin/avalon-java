20362e5b6271d0b0a0f89cadf77db9ab5b6abab0
avikulin <asvikulin@mail.ru>
2021-11-11 03:00:29 +0300

    Lab 1. Problem 2 is solved. Ready for CR.

---
+++ practices/Lab1-problem2/src/main/java/models/Publisher.java
@@ -0,0 +1,39 @@
+package models;
+
+public class Publisher {
+    private String name;
+    private String location;
+
+    public Publisher(String name, String location) {
+        setName(name);
+        setLocation(location);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if ((name == null) || name.isEmpty()) {
+            throw new IllegalArgumentException("Name must be set to non-blank value");
+        }
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        if ((location == null) || location.isEmpty()) {
+            throw new NullPointerException("Location must be set to non-blank value");
+        }
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s", name, location);
+    }
+
+}
